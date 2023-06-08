297087ac71a59afdda9c1d335c56df01af0d16b4
Ahmed Abuelenin <ahprogsolutions@gmail.com>
2023-06-08 17:54:50 +0300

    Feat(SearchReset): Added delete icon to SearchModal component to reset the input

--- src/sections/Header/SearchModal/SearchModal.js
+++ src/sections/Header/SearchModal/SearchModal.js
@@ -1,7 +1,8 @@
-import {memo, useState} from 'react'
+import {memo, useCallback, useState} from 'react'
 import {IoIosSearch as SearchIcon} from 'react-icons/io'
 import {Link} from 'react-router-dom'
 import './SearchModal.scss'
+import {GrFormClose as CloseIcon} from 'react-icons/gr'
 
 const SearchModal = ({isVisible}) => {
   console.log('SearchModal is rendering')
@@ -10,17 +11,31 @@ const SearchModal = ({isVisible}) => {
 
   const wrapperClass = isVisible ? 'search-modal-wrapper--visible' : ''
 
+  const resetInput = useCallback(() => {
+    setKeywords('')
+  }, [])
+
   return (
     <div className={`search-modal-wrapper ${wrapperClass}`}>
       <div className='search-modal'>
-        <input
-          type='text'
-          name='searchProduct'
-          placeholder='Search products'
-          value={keywords}
-          onChange={event => setKeywords(event.target.value)}
-          className='search-modal__input'
-        />
+        <div className='search-modal__input-container'>
+          <input
+            type='text'
+            name='searchProduct'
+            placeholder='Search products'
+            value={keywords}
+            onChange={event => setKeywords(event.target.value)}
+            className='search-modal__input'
+          />
+          {keywords.length > 0 ? (
+            <CloseIcon
+              color='black'
+              size='24'
+              onClick={resetInput}
+              className='search-modal__delete-icon'
+            />
+          ) : null}
+        </div>
         <Link to='#' className='global-button search-modal__search-link'>
           <SearchIcon title='Search for a product' size={28} />
         </Link>
