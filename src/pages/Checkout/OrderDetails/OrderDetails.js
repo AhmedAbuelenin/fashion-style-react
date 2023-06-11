a5fb3c32008182a2b86978e8ad05b32217f9041f
Ahmed Abuelenin <ahprogsolutions@gmail.com>
2023-06-11 15:09:35 +0300

    Fix(OrderTotals): Fixed wrong item subtotal in OrderTotals component

--- src/pages/Checkout/OrderDetails/OrderDetails.js
+++ src/pages/Checkout/OrderDetails/OrderDetails.js
@@ -24,7 +24,7 @@ const OrderDetails = props => {
               />
               <span className='order-totals__quantity'>{item.quantity}</span>
             </td>
-            <td>${item.price}</td>
+            <td>${item.price * item.quantity}</td>
           </tr>
         ))}
       </tbody>
