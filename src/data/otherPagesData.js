1d738e9b72f404bb5210f246ac75f5cf7aafb513
Ahmed Abuelenin <ahprogsolutions@gmail.com>
2023-06-26 17:23:14 +0300

    Fix(Navigation): Fixed wrong navigation route when click on shipping, terms and policy

--- src/data/otherPagesData.js
+++ src/data/otherPagesData.js
@@ -1,5 +1,5 @@
 export const otherPagesData = [
-  {id: 1, name: 'Shipping', path: '/#'},
-  {id: 2, name: 'Terms', path: '/#'},
-  {id: 3, name: 'Policy', path: '/#'}
+  {id: 1, name: 'Shipping', path: '#'},
+  {id: 2, name: 'Terms', path: '#'},
+  {id: 3, name: 'Policy', path: '#'}
 ]
